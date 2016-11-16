b57fe9d4785ee7c299baef1304286a05ba43b66c
Lukas <lukas.helmig@greselius.net>
2016-11-16 20:45:33 +0100

    1

--- menu.js
+++ menu.js
@@ -6,7 +6,7 @@ function myFunction() {
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdowncontent");
+    var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
